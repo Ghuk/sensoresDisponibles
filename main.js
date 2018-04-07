7848405b828c73f436c3e46633c1480a1d514d7c
Camilo A. Ramirez A <Camilo.1219@hotmail.com>
2018-04-07 17:29:18 -0500

    Prueba basica de sensores disponibles

---
+++ main.js
@@ -0,0 +1,51 @@
+function avaliableSensors(){
+
+	var featuresWindow = [{api:"Accelerometer",name:"acelerometro"},{api:"ProximitySensor","proximidad"},{api:"Gyroscope",name:"giroscopio"},
+							{api:"Magnetometer",name:"magnetometro"},{api:"AmbientLightSensor",name:"luz"}];
+	var featuresNavigator=  [{api:"getUserMedia",name:"camara"},{api:"bluetooth",name:"bluetooth"}];
+
+	for(var i = 0; i < featuresWindow.length; i++){
+		if (window.featuresWindow[i].api) { //Sensor acelerometro
+			document.getElementById(featuresWindow[i].name).innerText = 'Soportado';
+		}else{innerText(document.getElementById(featuresWindow[i].name).innerText = 'No Soportado';);}
+	}
+	for(var i = 0; i < featuresNavigator.length; i++){
+		if (navigator.featuresNavigator[i].api) { //Sensor acelerometro
+			document.getElementById(featuresNavigator[i].name).innerText = 'Soportado';
+		}else{innerText(document.getElementById(featuresNavigator[i].name).innerText = 'No Soportado';);}
+	}
+
+    /*
+	if (window.Accelerometer) { //Sensor acelerometro
+	  innerText('acelerometro','Soportado');
+	}else{innerText('acelerometro','No Soportado');}
+
+	if (window.ProximitySensor) { //Sensor de proximidad
+	  innerText('proximidad','Soportado');
+	}else{innerText('proximidad','No Soportado');}
+
+	if (window.Gyroscope) { // Sensor giroscopio
+	  innerText('giroscopio','Soportado');
+	}else{innerText('giroscopio','No Soportado');}
+
+	if (window.Magnetometer) { // Sensor magnetometro
+	  innerText('magnetometro','Soportado');
+	}else{innerText('magnetometro','No Soportado');}
+
+	if(navigator.getUserMedia){ // Camara
+		 innerText('camara','Soportado');
+	}else{innerText('camara','No Soportado');}
+
+	if(window.AmbientLightSensor){ // Sensor de luz
+		innerText('luz','Soportado');
+	}else{innerText('luz','No Soportado');}
+
+	if(navigator.bluetooth){ // Bluetooth
+		innerText('bluetooth','Soportado');
+	}else{innerText('bluetooth','No Soportado');}
+	*/
+}
+/*
+function innerText(elementId,text){
+	document.getElementById(elementId).innerText = text;
+}/*
